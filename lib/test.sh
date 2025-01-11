2c02e653be2b6284bfb2ea47446aed3500ace6a1
rylabs-billy <bthompso@akamai.com>
2025-01-11 14:32:17 -0500

    certbot command in test.sh

--- lib/test.sh
+++ lib/test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -x
 
-certbot --config "${config_file}" -vvv
+certbot certonly --config "${config_file}" -vvv
 
 # certbot -vvv -n --standalone --agree-tos --redirect certonly \
 #   -d "${DOMAIN}" -d "${SUBDOMAIN}.${DOMAIN}" -m "${SOA_EMAIL_ADDRESS}"
