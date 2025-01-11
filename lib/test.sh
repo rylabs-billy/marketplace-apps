7a4718a5bab37b1d9a1e495a6269a763352c7664
rylabs-billy <bthompso@akamai.com>
2025-01-11 16:02:11 -0500

    certbot command in test.sh

--- lib/test.sh
+++ lib/test.sh
@@ -3,7 +3,7 @@ set -x
 
 curl -ik https://localhost:14000/dir
 
-certbot certonly --config "${config_file}" -vvv
+REQUESTS_CA_BUNDLE="${ca_bundle}" certbot certonly --config "${config_file}" -vvv
 
 # certbot -vvv -n --standalone --agree-tos --redirect certonly \
 #   -d "${DOMAIN}" -d "${SUBDOMAIN}.${DOMAIN}" -m "${SOA_EMAIL_ADDRESS}"
